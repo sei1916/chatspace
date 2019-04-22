0f311b8299bb589ad33edd20b4f6d0e2262c78e7
sei1916 <pinodas1916@gmail.com>
2019-04-22 13:41:14 +0900

    Create_js

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,18 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.where('name LIKE(?) and id != ?', "%#{params[:keyword]}%" ,current_user)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email)
   end
+  
 end
