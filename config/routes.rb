1e52c037c35a1f6a02bc781e59892e2dd7c739aa
sei1916 <pinodas1916@gmail.com>
2019-04-22 15:28:14 +0900

    edit_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 # ルーティングを追加
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
   devise_for :users
   root "groups#index"
   resources :users, only: [:index, :edit, :update]
@@ -10,13 +9,7 @@ Rails.application.routes.draw do
     namespace :api do
       resources :messages, only: :index, defaults: {format: 'json'}
     end
-=======
-devise_for :users
-   root 'groups#index'
-  resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create]
->>>>>>> Stashed changes
+
   end
 end
 
